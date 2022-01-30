e6c2b826eb79baac1a2face2631eb712e83aa861
MelihKayali <melihkayali.35@gmail.com>
2022-01-30 20:53:05 +0300

    Day3 homework3.3 finished

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer added by ıd : " + customer.Id);
+        }
+
+        public void delete(Customer customer)
+        {
+            Console.WriteLine("Customer deleted by ıd : " + customer.Id);
+        }
+
+        public void getAll(Customer[] customers)
+        {   
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Id + " " + customer.FirstName + " " + customer.LastName + " " + customer.IdentityNumber);
+            }
+            Console.WriteLine("-----");
+            
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Ahmet";
+            customer.LastName = "Hamet";
+            customer.IdentityNumber = "12434523792222";
+
+
+            Customer customer1 = new Customer();
+            customer1.Id = 2;
+            customer1.FirstName = "Mehmet";
+            customer1.LastName = "Dürümcü";
+            customer1.IdentityNumber = "124345223435331222";
+
+
+            Customer customer2 = new Customer();
+            customer2.Id = 3;
+            customer2.FirstName = "Zeliha";
+            customer2.LastName = "Aynacı";
+            customer2.IdentityNumber = "124345232951222";
+            
+
+            Customer[] customers = new Customer[] { customer , customer1, customer2 };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer);
+            customerManager.delete(customer);
+            customerManager.getAll(customers);
+        }
+    }
+}
