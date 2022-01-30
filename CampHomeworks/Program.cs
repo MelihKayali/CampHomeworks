b3a478b1fac3d5eebd7ff00e3e399e751c4ef937
MelihKayali <melihkayali.35@gmail.com>
2022-01-30 19:59:12 +0300

    Add project files.

---
+++ CampHomeworks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CampHomeworks
+    // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my homework repo!");
+        }
+    }
+}
