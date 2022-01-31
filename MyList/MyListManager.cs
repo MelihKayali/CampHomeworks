218a6c1665fc3fe2f6fe5fa2e517417ee5f18a33
MelihKayali <melihkayali.35@gmail.com>
2022-01-31 22:45:15 +0300

    Day4 homework5

---
+++ MyList/MyListManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    internal class MyListManager<T>
+    {
+        T[] items;
+        T[] tempItems;
+        public MyListManager()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            tempItems = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempItems.Length; i++)
+            {
+                items[i] = tempItems[i];
+            }
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyList// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyListManager<string> myListManager = new MyListManager<string>();
+            myListManager.Add("İstanbul");
+            myListManager.Add("Ankara");
+        }
+    }
+}
