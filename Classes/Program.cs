3874fc3e66c364cc849166d9f74708e12d77973d
MelihKayali <melihkayali.35@gmail.com>
2022-02-02 18:24:03 +0300

    D5HW1

---
+++ Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Classes // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Melih";
+            customer.LastName = "Kayalı";
+            customer.City = "Ankara";
+
+            Customer customer2 = new Customer {Id= 2 , FirstName = "Yusuf" , LastName="Ağcı" , City="Ankara" };
+
+            Console.WriteLine(customer2.FirstName);
+        }
+
+    }    
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Interfaces // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonManager personManager = new PersonManager();
+            personManager.Add(new Customer { FirstName = "Melih" , Adress = "Beşiktaş" , Id = 1, LastName="Kayalı" });
+        }
+    }
+
+     interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+
+    class Customer : IPerson
+    {
+        public int Id { get ; set ; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+    }
+
+    class Person : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName);
+        }
+    }
+}
