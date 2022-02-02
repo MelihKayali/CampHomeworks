fa0adb4042e6e5660be509783c9c4fd0fd59cff8
MelihKayali <melihkayali.35@gmail.com>
2022-02-02 21:17:24 +0300

    D5HW2

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace InterfacesDemo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPersonManager customerManager = new CustomerManager();
+
+            IPersonManager employeeManager = new EmployeeManager();
+            
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(employeeManager);
+        }
+    }
+
+    interface IPersonManager
+    {
+         void Add();
+        void Update();
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Müşteri eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi.");
+        }
+    }
+
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Personel eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel güncellendi.");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            
+            personManager.Add();
+        }
+    }
+
+}
